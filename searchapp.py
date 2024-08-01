82023b0ac3a0f755e6cc70c446b51826af8ab60f
Explosivedoor <42119119+Explosivedoor@users.noreply.github.com>
2024-08-02 01:02:02 +0900

    Changed to working with a database instead
    Sped up app by loading it as a database to a dataframe. Implemented a cache function but I am unsure if it is implemented correctly.

--- searchapp.py
+++ searchapp.py
@@ -2,59 +2,42 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import sqlite3
+import json
+import re
 
+st.title("Car Terms Search")
 conn = sqlite3.connect('carterms.db')
 cursor = conn.cursor()
-#creates table if it isn't there
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS terms (
-        Japanese TEXT ,
-        English  TEXT
-    )
-''')
 
 
 
 @st.cache_data(show_spinner =True)
-def load_data(file_path):
+def load_data():
     # Read the Excel file
-    df = pd.read_excel(file_path)
+    df = pd.read_sql_query("SELECT * FROM terms", conn)
+    #df = pd.DataFrame({ "Japanese": json_object.keys(), 'English': json_object.values()})
     return df
-df = load_data("D:\\Downloads\\Auction-Sheet-Terms.xlsx")
-
-columns_to_drop = range(0, 3)
-rows_to_drop = range(0,17)
-df.drop(df.columns[columns_to_drop], axis=1, inplace=True)
-df.drop(rows_to_drop, axis=0, inplace=True)
-df.reset_index(drop=True, inplace=True)
+df = load_data()
+#st.dataframe(df)
+    
 En={}
 Jp={}
-df.columns = ['Jp', 'En', 'Mg', 'Sp','Cat']
-
+df.columns = ['Jp', 'En']
 toggle_label = (
     "English to Japanese"
     if st.session_state.get("my_toggle", False)
     else "Japanese to English"
 )
 toggle_value = st.session_state.get("my_toggle", False)
-for i in range(len(df)):
-    x = df.iloc[i,0]
-    y = df.iloc[i,1]
-    query = '''INSERT INTO terms (Japanese, English) 
-                    VALUES (?, ?)'''
 
-    # Execute the query with the provided values
-    cursor.execute(query, (x, y))
-    conn.commit()
 
 for i in range(len(df)):
         x = df.iloc[i,0]
         y = df.iloc[i,1]
         En[x] = y
         Jp[y] = x
- 
-       
+
+
 on = st.toggle(toggle_label, value = toggle_value,key="my_toggle")
 if on == True:
     
@@ -67,12 +50,6 @@ else:
         print_lang = 1
         dic = Jp
 
-#st.write(test2) 
-#st.dataframe(df)
-
-#st.write(df.iloc[0,0]) 
-
-test_key = dic.keys()
 test_values = dic.values()
 
 option = st.selectbox(
@@ -81,11 +58,13 @@ option = st.selectbox(
     placeholder="<search here>", key = "tim"
 )
 try:
-    filtered_rows = df[df[lang].str.contains(option, case=False, na=False)]
+    escaped_option = re.escape(option)
+   
+    pattern = f"({escaped_option})"
+    
+    filtered_rows = df[df[lang].str.contains(pattern, case=False, na=False)]
     #st.write(filtered_rows)
     for i in df.iloc[filtered_rows.index,print_lang]:
         st.write(i)
 except:
     st.write("No Result")
-
-
